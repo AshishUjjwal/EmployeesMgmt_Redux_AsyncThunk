ef5bcab9d7b91a5c9f0e95efc6ceb20a4e7dea39
Ashish Ujjwal <98599848+AshishUjjwal@users.noreply.github.com>
2025-03-11 09:19:15 +0000

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* <Route exact path="/" element={<Create />} />
+          <Route exact path="/read" element={<Read />} />
+          <Route exact path="/edit/:id" element={<Update />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
