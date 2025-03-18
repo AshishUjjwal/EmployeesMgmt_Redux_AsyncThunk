625482b6ce3b4f5af69783c187c4317bf6348c61
Ashish Ujjwal <98599848+AshishUjjwal@users.noreply.github.com>
2025-03-18 18:02:36 +0000

    File structure changes

---
+++ frontend/src/Redux/Userfeature/Actions/createUserAction.js
@@ -0,0 +1,9 @@
+import {createAsyncThunk, isRejectedWithValue} from "@reduxjs/toolkit";
+
+// Show User Action
+export const showUser = createAsyncThunk(
+    "showUser",
+    async(args, {rejectWithValue}) =>{
+
+    }
+)
